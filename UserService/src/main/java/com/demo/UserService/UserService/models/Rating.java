b49edea59047b3f754134785a6a99892335740d1
Harish Jangam <jangamharish949@gmail.com>
2026-06-17 23:35:07 +0530

    started implementing microservice

---
+++ UserService/src/main/java/com/demo/UserService/UserService/models/Rating.java
@@ -0,0 +1,11 @@
+package com.demo.UserService.UserService.models;
+
+public class Rating {
+    private String ratingId;
+    private String userId;
+    private String hotelId;
+    private int rating;
+    private String feedback;
+    private Hotel hotel;
+
+}
---
+++ UserService/src/main/java/com/demo/UserService/UserService/models/User.java
@@ -0,0 +1,26 @@
+package com.demo.UserService.UserService.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name="ID")
+    private String userId;
+    @Column(name = "NAME", length = 20)
+    private String name;
+    private String email;
+    private String about;
+    @Transient
+    private List<Rating> ratings=new ArrayList<>();
+}
